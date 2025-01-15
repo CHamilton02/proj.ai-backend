1a8b6a2dc5fb5fdde2b9f1d359fc0ce97e66db70
Camdyn Hamilton <c02hamil@gmail.com>
2025-01-14 20:18:50 -0500

    feat: ensure request body is valid

--- src/routes/project.js
+++ src/routes/project.js
@@ -1,12 +1,18 @@
 import express from "express";
 import OpenAI from "openai";
 import { zodResponseFormat } from "openai/helpers/zod";
-import { Project } from "../models/Project";
+import { Project } from "../models/Project.js";
 
 const router = express.Router();
 const openai = new OpenAI();
 
 router.post("/generate", async (req, res) => {
+  if (req.body.topics === undefined || req.body.difficulty === undefined) {
+    res
+      .status(400)
+      .send("Invalid body! Ensure topics and difficulty are included.");
+  }
+
   const completion = await openai.chat.completions.create({
     model: "gpt-4o-mini",
     messages: [
